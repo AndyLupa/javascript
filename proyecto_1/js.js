b8a4685009e6c4986774fdc9c132c0fd34894d08
AndyLupa <87453513+AndyLupa@users.noreply.github.com>
2022-04-17 22:02:02 -0300

    proyecto_2

--- proyecto_1/js.js
+++ proyecto_1/js.js
@@ -39,17 +39,41 @@ function calcular_area_cuadrdo(){
 }
 
 function calcular_perimetro_triangulo(){
-    let valor1 = document.querySelector("#inputTriangulo").value;
     let valor2 = document.querySelector("#inputTriangulo2").value;
     let valor3 = document.querySelector("#inputTriangulo3").value;
-    alert("Perímetro Triánguulo:" + perimetro_triangulo(valor1,valor2,valor3));
     
+    if(VerificoIsoceles(valor2,valor3)==1){    
+        alert("Perímetro Triánguulo:" + perimetro_triangulo(valor2,valor2,valor3));
+    }
+    else{
+        alert("El triángulo debe ser Isosceles, dos lados iguales y uno distinto");
+    }
+    
+}
+
+function VerificoIsoceles(l,b){
+    if(l==b){
+        return 0;
+    }
+    else{
+        return 1;
+    }
 }
 
 function calcular_area_triangulo(){
     let valor3 = document.querySelector("#inputTriangulo3").value;
-    let altura = document.querySelector("#inputTrianguloAltura").value;
-    alert("Área Triánguulo:" + area_triangulo(valor3,altura));    
+    let valor2 = document.querySelector("#inputTriangulo2").value;
+    
+    if(VerificoIsoceles(valor2,valor3)==1){    
+        let altura = document.querySelector("#inputTrianguloAltura").value;
+        alert("Área Triánguulo:" + area_triangulo(valor3,altura));  
+    }
+    else{
+        alert("El triángulo debe ser Isosceles, dos lados iguales y uno distinto");
+    }
+    
+
+     
 }
 
 
